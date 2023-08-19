1e30686d9fe3693da264876606bdf2db0bed042c
shivam kumar singh <45334414+shivsingh001@users.noreply.github.com>
2023-08-19 11:37:44 +0530

    First Version of my Food application

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import logo from "../Images/logo.png";
+
+
+const Title = () => (
+    <a href="/">
+        <img
+            className="logo"
+            alt="logo"
+            src={logo} />
+
+
+    </a>
+)
+const Header = function () {
+    return (
+
+        <div className="header">
+            <Title />
+            <div className="nav-items">
+                <ul>
+                    <li> Home</li>
+                    <li> About</li>
+                    <li> Contact</li>
+                    <li> Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+
+export default Header;
